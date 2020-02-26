e59e28eb522759368e4d60b3c757fcf2df809534
Negorizont <boltuev01@mail.ru>
2020-02-26 22:35:05 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    if (!matrix) {
+        return [];
+    }
+    
+    if (matrix.length === 0) {
+      return [];
+    }
+
+  return matrix.reduce(function(sum, item, i) {
+    if ((i+1)%2 == 0) {
+        sum.push(...(item.reverse()));
+    } else {
+        sum.push(...item);
+    }
+    return sum;
+  });
 }
